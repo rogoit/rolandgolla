2d8df35e70bc064c505a77be5be2e26d468dcfff
Roland Golla <rolandgolla@gmail.com>
2016-09-18 22:41:49 +0200

    exclude mouseOverAndClick on phantomjs

--- tests/acceptance/navigationCest.php
+++ tests/acceptance/navigationCest.php
@@ -1,5 +1,6 @@
 <?php
 use Step\Acceptance\Acceptance;
+use \Codeception\Scenario;
 
 class navigationCest
 {
@@ -9,49 +10,54 @@ class navigationCest
         $I->waitForElement('.header');
     }
 
-    //public function checkLinksToAnchor(Acceptance $I)
-    //{
-    //    $links = $I->grabMultiple('.header-navigation > li > a', 'href');
-    //    foreach ($links as $link) {
-    //        $I->assertContains('#', $link, 'Link contains #: ' . $link);
-    //    }
-    //}
-    //
-    //public function checkLogoLink(Acceptance $I)
-    //{
-    //    $selector = '.site-logo';
-    //
-    //    $href = $I->grabAttributeFrom($selector, 'href');
-    //    $I->assertContains('http', $href);
-    //
-    //    $target = $I->grabAttributeFrom($selector, 'target');
-    //    $I->assertContains('_blank', $target);
-    //
-    //    $title = $I->grabAttributeFrom($selector, 'title');
-    //    $I->assertNotEmpty('_blank', $title);
-    //}
-    //
-    //public function resizeNavigation(Acceptance $I)
-    //{
-    //    $burgerMenu = '.mobi-toggler';
-    //    $headerNavigation = '.header-navigation';
-    //
-    //    $I->dontSeeElement($burgerMenu);
-    //    $I->resizeWindow(400, 1000);
-    //    $I->wait(1);
-    //    $I->waitForElement($burgerMenu);
-    //    $I->cantSeeElement($headerNavigation);
-    //    $I->click($burgerMenu);
-    //    $I->waitForElement($headerNavigation);
-    //    $I->click($burgerMenu);
-    //    $I->waitForElementNotVisible($headerNavigation);
-    //    $I->resizeWindow(1600, 1000);
-    //    $I->wait(1);
-    //    $I->waitForElementNotVisible($burgerMenu);
-    //}
-
-    public function mouseOverAndClick(Acceptance $I)
+    public function checkLinksToAnchor(Acceptance $I)
     {
+        $links = $I->grabMultiple('.header-navigation > li > a', 'href');
+        foreach ($links as $link) {
+            $I->assertContains('#', $link, 'Link contains #: ' . $link);
+        }
+    }
+
+    public function checkLogoLink(Acceptance $I)
+    {
+        $selector = '.site-logo';
+
+        $href = $I->grabAttributeFrom($selector, 'href');
+        $I->assertContains('http', $href);
+
+        $target = $I->grabAttributeFrom($selector, 'target');
+        $I->assertContains('_blank', $target);
+
+        $title = $I->grabAttributeFrom($selector, 'title');
+        $I->assertNotEmpty('_blank', $title);
+    }
+
+    public function resizeNavigation(Acceptance $I)
+    {
+        $burgerMenu = '.mobi-toggler';
+        $headerNavigation = '.header-navigation';
+
+        $I->dontSeeElement($burgerMenu);
+        $I->resizeWindow(400, 1000);
+        $I->wait(1);
+        $I->waitForElement($burgerMenu);
+        $I->cantSeeElement($headerNavigation);
+        $I->click($burgerMenu);
+        $I->waitForElement($headerNavigation);
+        $I->click($burgerMenu);
+        $I->waitForElementNotVisible($headerNavigation);
+        $I->resizeWindow(1600, 1000);
+        $I->wait(1);
+        $I->waitForElementNotVisible($burgerMenu);
+    }
+
+    public function mouseOverAndClick(Acceptance $I, Scenario $scenario)
+    {
+        if ($scenario->current('browser') == 'phantomjs') {
+            $I->comment('Skip on phantomjs');
+            return;
+        }
+
         $selector = '.header-navigation > li';
         $navItems = $I->grabMultiple($selector, 'class');
 
