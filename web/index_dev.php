34239b3aca03bdfe4900c6080ff057fead54e069
Roland Golla <rolandgolla@gmail.com>
2017-01-10 23:15:12 +0100

    index dev back to repo

---
+++ web/index_dev.php
@@ -0,0 +1,25 @@
+<?php
+
+$filename = __DIR__.preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']);
+if (php_sapi_name() === 'cli-server' && is_file($filename)) {
+    return false;
+}
+
+use Symfony\Component\Debug\Debug;
+
+if (isset($_SERVER['HTTP_CLIENT_IP'])
+    || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
+    || !in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1'))
+) {
+    header('HTTP/1.0 403 Forbidden');
+    exit('You are not allowed to access this file. Check ' . basename(__FILE__) . ' for more information.');
+}
+
+require_once __DIR__.'/../vendor/autoload.php';
+Debug::enable();
+
+$app = new App\Application('dev');
+
+require __DIR__.'/../config/dev.php';
+require __DIR__.'/../src/controllers.php';
+$app->run();
